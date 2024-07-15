b8d453bb2ab704b8535622cb58969c6caa4f4ef3
KOSASIH <kosasihg88@gmail.com>
2024-07-15 21:21:17 +0700

    Create interface_base.py

---
+++ src/interfaces/interface_base.py
@@ -0,0 +1,11 @@
+class InterfaceBase:
+    def __init__(self):
+        pass
+
+    def send(self, data):
+        # TO DO: Implement sending logic
+        pass
+
+    def receive(self):
+        # TO DO: Implement receiving logic
+        pass
