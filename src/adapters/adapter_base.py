9b3d9d5f0d5aec77bbe9b82da185b74165c06ca5
KOSASIH <kosasihg88@gmail.com>
2024-07-15 21:18:58 +0700

    Create adapter_base.py

---
+++ src/adapters/adapter_base.py
@@ -0,0 +1,11 @@
+class AdapterBase:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        # TO DO: Implement connection logic
+        pass
+
+    def disconnect(self):
+        # TO DO: Implement disconnection logic
+        pass
