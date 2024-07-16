6d7bb4e7e6ce39bfa0551f617052807e52dd0d59
KOSASIH <kosasihg88@gmail.com>
2024-07-16 10:02:40 +0700

    Create artificial_creativity_module.py

---
+++ src/utils/artificial_creativity_module.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class ArtificialCreativityModule:
+    def __init__(self, knowledge_graph):
+        self.knowledge_graph = knowledge_graph
+
+    def generate_creative_idea(self, problem):
+        # TO DO: Implement artificial creativity generation algorithm
+        pass
+
+    def refine_creative_idea(self, idea, feedback):
+        # TO DO: Implement artificial creativity refinement algorithm
+        pass
