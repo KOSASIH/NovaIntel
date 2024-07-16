d4dbd5b85f3e6d856778ca167f3e7bedfaf1ef52
KOSASIH <kosasihg88@gmail.com>
2024-07-16 09:21:36 +0700

    Create transdimensional_navigation_v2.py

---
+++ src/utils/transdimensional_navigation_v2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class TransdimensionalNavigationV2:
+    def __init__(self, transdimensional_map):
+        self.transdimensional_map = transdimensional_map
+
+    def navigate_v2(self, destination):
+        # TO DO: Implement transdimensional navigation algorithm v2
+        pass
+
+    def plot_course_v2(self, course):
+        # TO DO: Implement transdimensional course plotting algorithm v2
+        pass
